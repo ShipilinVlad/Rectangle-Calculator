9be089e3b2c2a130596e988d4ed0012d0f6437c7
Vladislav Shipilin <ShipilinVladislav@yandex.ru>
2024-01-23 09:11:31 +0700

    Импорт нужных библиотек

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QGraphicsScene,
+                             QGraphicsView, QGraphicsRectItem,
+                             QVBoxLayout, QLabel, QLineEdit, QPushButton)
+from PyQt5.QtGui import QColor, QBrush
